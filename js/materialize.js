41209018bb49a2634fee5e7c67e6c7ee65c59956
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-25 09:21:16 -0600

    playing with push and pull of order form cards

--- js/materialize.js
+++ js/materialize.js
@@ -7,6 +7,13 @@
  $(document).ready(function(){
    console.log('Ready to Rock!')
    $('.parallax').parallax();
+
+   $('.fixed-action-btn').openFAB();
+   $('.fixed-action-btn').closeFAB();
+   $('.fixed-action-btn.toolbar').openToolbar();
+   $('.fixed-action-btn.toolbar').closeToolbar();
+
+
  });
 
 // Check for jQuery.
