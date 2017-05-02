ab9f5af04911b00e4f28d5fbfd009310571b0c27
Shannon Rivers <shannon.rivers@gmail.com>
2017-05-01 19:04:16 -0600

    order recap 90%: need totals lines and tax

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   console.log('Ready to Rock!');
+  $("#footer").hide();
   $(".button-collapse").sideNav({
     menuWidth: 300,
     edge: 'right',
@@ -7,40 +8,87 @@ $(document).ready(function(){
     });
 
 
+  var totalQty = 0;
+  var subtotal = 0;
   $(".ramens").click(function (event){
+    $("#footer").show();
     var ramenType = ((event.target).className).slice(15);
+    var orderRow = $("<tr>");
+    var ramenDetail = $("<td>");
+    var qtyDetail = $("<td>");
+    var priceDetail = $("<td>");
       switch (ramenType) {
-        case butao:
-          var orderRow = $("<tr>");
+        case "butao":
           orderRow.className = ramenType;
-          var ramenDetail = $("<td>");
-          ramenDetail.attr("class", "col s6 offset-s1");
+          ramenDetail.attr("class", "col s5 offset-s1");
           ramenDetail.text("Original King (buta-o) ramen");
           ramenDetail.appendTo(orderRow);
-          var qtyDetail = $("<td>");
-          qtyDetail.attr("class", "col s2");
-          qtyDetail.text("ichi (-)");
+          qtyDetail.attr("class", "col s3 centered");
+          qtyDetail.text("ichi (1)");
+          totalQty += 1;
           qtyDetail.appendTo(orderRow);
-          var priceDetail = $("<td>");
-          priceDetail.attr("class", "col s2 offset-s1");
+          priceDetail.attr("class", "col s3");
           priceDetail.text("$12.99");
+          subtotal += 12.99;
           priceDetail.appendTo(orderRow);
           orderRow.appendTo($("tbody"));
           break;
+
+        case "kuroo":
+          orderRow.className = ramenType;
+          ramenDetail.attr("class", "col s5 offset-s1");
+          ramenDetail.text("Black King (kuro-o) ramen");
+          ramenDetail.appendTo(orderRow);
+          qtyDetail.attr("class", "col s3 centered");
+          qtyDetail.text("ichi (1)");
+          totalQty += 1;
+          qtyDetail.appendTo(orderRow);
+          priceDetail.attr("class", "col s3");
+          priceDetail.text("$13.99");
+          subtotal += 13.99;
+          priceDetail.appendTo(orderRow);
+          orderRow.appendTo($("tbody"));
+          break;
+
+        case "akao":
+          orderRow.className = ramenType;
+          ramenDetail.attr("class", "col s5 offset-s1");
+          ramenDetail.text("Red King (aka-o) ramen");
+          ramenDetail.appendTo(orderRow);
+          qtyDetail.attr("class", "col s3 centered");
+          qtyDetail.text("ichi (1)");
+          totalQty += 1;
+          qtyDetail.appendTo(orderRow);
+          priceDetail.attr("class", "col s3");
+          priceDetail.text("$13.99");
+          subtotal += 13.99;
+          priceDetail.appendTo(orderRow);
+          orderRow.appendTo($("tbody"));
+          break;
+
         default:
+          orderRow.className = ramenType;
+          ramenDetail.attr("class", "col s5 offset-s1");
+          ramenDetail.text("Green King (midori-o) ramen");
+          ramenDetail.appendTo(orderRow);
+          qtyDetail.attr("class", "col s3 centered");
+          qtyDetail.text("ichi (1)");
+          totalQty += 1;
+          qtyDetail.appendTo(orderRow);
+          priceDetail.attr("class", "col s3");
+          priceDetail.text("$12.99");
+          subtotal += 12.99;
+          priceDetail.appendTo(orderRow);
+          orderRow.appendTo($("tbody"));
+          break;
+      }//closes ramen switch
+      console.log(totalQty);
+      console.log(subtotal);
+      $("#totalQty").text += totalQty;
+      $("subtotal").text += subtotal;
+    });//closes ramens.click function
 
-      }
 
-
-    var orderRow = $("<tr>");
-    var orderDetail = $("<td>");
-    orderDetail.attr("class", "col s6 offset-s1")
-    orderRow.className += ` ${ramenType}`;
-    orderDetail.text(ramenType);
-    orderDetail.appendTo(orderRow);
-    console.log(orderRow);
-    orderRow.appendTo($("tbody"));
-  });
 
   $("#submitBtn").click(function (event){
     event.preventDefault();
