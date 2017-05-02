d619cd5dc744242cf0261748e9fc5668c4917b8b
Shannon Rivers <shannon.rivers@gmail.com>
2017-05-01 18:12:55 -0600

    happy-ish with menu and order form layout - working on order recap field population

--- js/main.js
+++ js/main.js
@@ -6,15 +6,52 @@ $(document).ready(function(){
     closeOnClick: true,
     });
 
-    $("#submitBtn").click(function (event){
-      event.preventDefault();
 
+  $(".ramens").click(function (event){
+    var ramenType = ((event.target).className).slice(15);
+      switch (ramenType) {
+        case butao:
+          var orderRow = $("<tr>");
+          orderRow.className = ramenType;
+          var ramenDetail = $("<td>");
+          ramenDetail.attr("class", "col s6 offset-s1");
+          ramenDetail.text("Original King (buta-o) ramen");
+          ramenDetail.appendTo(orderRow);
+          var qtyDetail = $("<td>");
+          qtyDetail.attr("class", "col s2");
+          qtyDetail.text("ichi (-)");
+          qtyDetail.appendTo(orderRow);
+          var priceDetail = $("<td>");
+          priceDetail.attr("class", "col s2 offset-s1");
+          priceDetail.text("$12.99");
+          priceDetail.appendTo(orderRow);
+          orderRow.appendTo($("tbody"));
+          break;
+        default:
+
+      }
+
+
+    var orderRow = $("<tr>");
+    var orderDetail = $("<td>");
+    orderDetail.attr("class", "col s6 offset-s1")
+    orderRow.className += ` ${ramenType}`;
+    orderDetail.text(ramenType);
+    orderDetail.appendTo(orderRow);
+    console.log(orderRow);
+    orderRow.appendTo($("tbody"));
+  });
+
+  $("#submitBtn").click(function (event){
+    event.preventDefault();
     });
 
+  $('select').material_select();
   $('.fixed-action-btn').openFAB();
   $('.fixed-action-btn').closeFAB();
-  $('.fixed-action-btn.toolbar').openToolbar();
-  $('.fixed-action-btn.toolbar').closeToolbar();
+
+
+
 
 
 });
